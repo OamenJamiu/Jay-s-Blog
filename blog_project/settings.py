3630bf0860f6ff23817a296ffd2c745db0e4a4ce
Oamen Jamiu <oamenjamiu@gmail.com>
2019-07-26 17:00:44 +0100

    Second commit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -95,16 +95,27 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # }
 
 
+
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mysite',
-        'USER': 'postgres',
-        'PASSWORD': 'abduljam',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mysite',
+#         'USER': 'postgres',
+#         'PASSWORD': 'abduljam',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
