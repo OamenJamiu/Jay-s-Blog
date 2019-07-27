73e6b6225bf94a035596924e724af445f154afcd
Oamen Jamiu <oamenjamiu@gmail.com>
2019-07-27 14:56:01 +0100

    Final

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -110,15 +110,6 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 
 
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
